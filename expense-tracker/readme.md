3562379f5dbc8c4687f2542b5ad027030e2d3324
endw <57078225+endw0901@users.noreply.github.com>
2021-04-26 19:47:20 +0900

    Add files via upload

---
+++ expense-tracker/readme.md
@@ -0,0 +1,87 @@
+# memo
+
+# CSS
+
+- マウスオーバーのときだけ × ボタンを表示したい
+
+```
+.delete-btn {
+   opacity: 0;
+}
+
+.list li:hover .delete-btn {
+  opacity: 1;
+}
+```
+
+## javascript
+
+- forEach で個別要素を引数に関数 call する際、引数が不要
+  ※forEach(callback 関数)
+
+```
+function addTransactionDOM(transaction) {
+
+transactions.forEach(addTransactionDOM);
+```
+
+- map() は配列から配列を作るメソッド
+
+```
+https://www.sejuku.net/blog/21812
+「forEach」は単純に実行するだけのメソッドなのに対して、「map」は実行後の結果を配列データとして返してくれるという点が違う
+
+map() は配列から配列を作るメソッドである
+もとの配列は変更されず、新しい配列を作る
+const array = [1, 2, 3]
+const newArray = array.map(value => 2 * value)
+
+console.log(newArray) // [ 2, 4, 6 ]
+```
+
+- toFixed(2) 小数点以下 2 桁（0 埋め) ※桁数処理はあいまいなので厳密には使わない
+- 累積 reduce
+
+```
+ const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+```
+
+- マイナスのものだけ累積: filter , reduce
+
+```
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+```
+
+- オブジェクトの値を数値にする
+
+```
+    // amountに+をつける：数値、つけない：文字列のオブジェクトになる
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value,
+    };
+
+```
+
+- filter で削除
+
+```
+transactions = transactions.filter((transaction) => transaction.id !== id);
+```
+
+### localStorage
+
+- 取り出す
+
+```
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem('transactions')
+);
+
+let transactions =
+  localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+```
---
+++ expense-tracker/script.js
@@ -0,0 +1,115 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+// const dummyTransactions = [
+//   { id: 1, text: 'Flower', amount: -20 },
+//   { id: 2, text: 'Salary', amount: 300 },
+//   { id: 3, text: 'Book', amount: -10 },
+//   { id: 4, text: 'Camera', amount: 150 },
+// ];
+
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem('transactions')
+);
+
+let transactions =
+  localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+function addTransaction(e) {
+  // submitをしないようにする
+  e.preventDefault();
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please add a text and amount');
+  } else {
+    // amountに+をつける：数値、つけない：文字列のオブジェクトになる
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value,
+    };
+    // console.log(transaction);
+
+    transactions.push(transaction);
+    addTransactionDOM(transaction); // transactionのリスト更新
+    updateValues(); // transactionsから値更新
+    updateLocalStorage();
+
+    text.value = '';
+    amount.value = '';
+  }
+}
+
+function generateID() {
+  return Math.floor(Math.random() * 1000000);
+}
+
+function addTransactionDOM(transaction) {
+  const sign = transaction.amount < 0 ? '-' : '+';
+  const item = document.createElement('li');
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span> <button class="delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
+  `;
+
+  list.appendChild(item);
+}
+
+function updateValues() {
+  // mapでtransactionの配列からamountの配列を作る
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  // console.log(amounts);
+  // console.log(total);
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  // console.log(income);
+  // console.log(expense);
+
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
+}
+
+function removeTransaction(id) {
+  // DOMから削除
+  transactions = transactions.filter((transaction) => transaction.id !== id);
+
+  updateLocalStorage();
+
+  init();
+}
+
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+function init() {
+  list.innerHTML = '';
+
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+init();
+
+form.addEventListener('submit', addTransaction);
